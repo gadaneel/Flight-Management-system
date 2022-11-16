a73d8140c670ebd1e328b340bcd7a477a7a5e74b
Akshay Gattani <88683233+agattani190@users.noreply.github.com>
2022-11-15 23:53:43 -0500

    Added validations for login and signup fields

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -17,14 +17,18 @@ export class LoginComponent implements OnInit {
   isLoginFailed = false;
   isAdmin = false;
   errorMessage = '';
+  isValidEmail = true;
+  isValidPassword = true;
+  emailValidationError = '';
+  passwordValidationError = '';
   roles: string[] = [];
   notification: string = "Note: ";
-  constructor(private authService: AuthService, private addNotificationService: AddNotificationService, private tokenStorage: TokenStorageService, private router:Router) { }
+  constructor(private authService: AuthService, private addNotificationService: AddNotificationService, private tokenStorage: TokenStorageService, private router: Router) { }
   ngOnInit(): void {
     if (this.tokenStorage.getToken()) {
       this.isLoggedIn = true;
       this.roles = this.tokenStorage.getUser().roles;
-      if (this.roles[0]=="ROLE_ADMIN") {
+      if (this.roles[0] == "ROLE_ADMIN") {
         this.isAdmin = true;
       }
       else {
@@ -33,14 +37,81 @@ export class LoginComponent implements OnInit {
             this.notification += data.message;
           },
           err => {
-            this.errorMessage = err.error.message;
+            this.errorMessage = err.error ? err.error.message : "";
           }
         );
       }
     }
+    // document.getElementById("username")?.addEventListener('keypress', (event) => {this.validateEmail();});
+    // document.getElementById("password")?.addEventListener('keypress', (event) => {this.validatePassword();});
   }
+  
+
+  validateEmail(): void {
+    const search = this.form.username;
+    if(search == null) {
+      this.emailValidationError = 'Username is required';
+      this.isValidEmail = false;
+      return;
+    }
+    const res = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/).test(search);
+    if(res) {
+      this.emailValidationError = '';
+    } else {
+      this.emailValidationError = 'Username is invalid';
+    }
+    this.isValidEmail = res;
+    return;
+  }
+
+  validatePassword(): void {
+    const search = this.form.password;
+    if(search == null) {
+      this.passwordValidationError = 'Password is required';
+      this.isValidPassword = false;
+      return;
+    }
+    if(search.length < 8) {
+      this.passwordValidationError = 'Password must contain at least 8 characters';
+      this.isValidPassword = false;
+      return;
+    }
+    if(search.length > 25) {
+      this.passwordValidationError = 'Password must contain at most 25 characters';
+      this.isValidPassword = false;
+      return;
+    }
+    if(search.toUpperCase() == search) {
+      this.passwordValidationError = 'Password must contain at least one lowercase character';
+      this.isValidPassword = false;
+      return;
+    }
+    if(search.toLowerCase() == search) {
+      this.passwordValidationError = 'Password must contain at least one uppercase character';
+      this.isValidPassword = false;
+      return;
+    }
+    if(!new RegExp(/\d/).test(search)) {
+      this.passwordValidationError = 'Password must contain at least one numeric character';
+      this.isValidPassword = false;
+      return;
+    }
+    if(!new RegExp(/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/).test(search)) {
+      this.passwordValidationError = 'Password must contain at least one special character';
+      this.isValidPassword = false;
+      return;
+    }
+    this.isValidPassword = true;
+    this.passwordValidationError = '';
+    return;
+  }
+
   onSubmit(): void {
     const { username, password } = this.form;
+    this.validateEmail();
+    this.validatePassword();
+    if(!this.isValidEmail || !this.isValidPassword)
+      return;
     this.authService.login(username, password).subscribe(
       data => {
         this.tokenStorage.saveToken(data.accessToken);
@@ -48,7 +119,7 @@ export class LoginComponent implements OnInit {
         this.isLoginFailed = false;
         this.isLoggedIn = true;
         this.roles = this.tokenStorage.getUser().roles;
-        if (this.roles[0]=="ROLE_ADMIN") {
+        if (this.roles[0] == "ROLE_ADMIN") {
           this.isAdmin = true;
           this.reloadPage();
         }
@@ -59,18 +130,19 @@ export class LoginComponent implements OnInit {
               this.reloadPage();
             },
             err => {
-              this.errorMessage = err.error.message;
+              this.errorMessage = err.error ? err.error.message : "";
             }
           );
         }
-        
+
       },
       err => {
-        this.errorMessage = err.error.message;
+        this.errorMessage = err.error ? err.error.message : "";
         this.isLoginFailed = true;
       }
     );
   }
+
   reloadPage(): void {
     window.location.reload();
   }
